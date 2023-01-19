287d90fdf31bd6d344aca3a8e0a8b3d7b86b9779
Yirui99 <yirsong@ucdavis.edu@github.com>
2023-01-19 09:40:10 -0800

    Staybooking

---
+++ com/laioffer/staybooking/exception/UserNotExistException.java
@@ -0,0 +1,18 @@
+package com.laioffer.staybooking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+public class UserNotExistException extends RuntimeException {
+    public UserNotExistException(String message) {
+        super(message);
+    }
+
+    @ExceptionHandler(UserNotExistException.class)
+    public final ResponseEntity<String> handleUserNotExistExceptions(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+}
---
+++ com/laioffer/staybooking/repository/CustomLocationRepository.java
@@ -0,0 +1,6 @@
+package com.laioffer.staybooking.repository;
+import java.util.List;
+
+public interface CustomLocationRepository {
+    List<Long> searchByDistance(double lat, double lon, String distance);
+}
